f43c75eb28918c9d96e37ec544443122ac00048f
Rick <3089645676@qq.com>
2023-01-14 17:38:18 +0800

    Initial commit

---
+++ src/com/java/GitTest.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+        System.out.println("hello, world!");
+    }
+}

60769713d5faca41d7c8d37156fbbb063d908e85
Ayun28 <60725429+Ayun28@users.noreply.github.com>
2023-01-14 17:57:21 +0800

    pull commit

--- src/com/java/GitTest.java
+++ src/com/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello, world!");
         System.out.println("hello, world!");
         System.out.println("hello, world!01");
+        System.out.println("pull test!");
     }
 }
